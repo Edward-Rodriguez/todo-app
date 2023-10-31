29e7f699c01c6f0f0585164682d6863acee0f800
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-10-31 15:50:12 -0400

    Add projects section to the nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,15 +4,16 @@ import Storage from '../../storage';
 import CalendarTodayIcon from './calendar_today_icon.svg';
 import CalendarIcon from './calendar_month_icon.svg';
 import CalendarUpcomingIcon from './event_upcoming_icon.svg';
+import CloseIconRed from '../../assets/img/close_icon_red.svg';
+import RedDotIcon from './reddish_fill.svg';
+import GreenDotIcon from './reddish_fill.svg';
+import PurpleDotcon from './reddish_fill.svg';
+import TurquoiseDotIcon from './reddish_fill.svg';
 import './nav.css';
 
 export const nav = () => {
   const storage = Storage();
   const nav = document.createElement('nav');
-  const allButton = document.createElement('button');
-  const todayButton = document.createElement('button');
-  const upcomingButton = document.createElement('button');
-  const projectsHeader = document.createElement('h1');
 
   const allLink = menuLink('All', storage.todos.length, CalendarIcon);
   const todayLink = menuLink(
@@ -30,6 +31,36 @@ export const nav = () => {
   );
   nav.setAttribute('id', 'sidebar-menu');
   nav.append(allLink, todayLink, upcomingLink);
+
+  // projects section
+  const projectsContainer = document.createElement('div');
+  const projectsHeader = document.createElement('header');
+  const heading = document.createElement('h1');
+  const fillIcons = [RedDotIcon, GreenDotIcon, PurpleDotcon, TurquoiseDotIcon];
+
+  projectsContainer.setAttribute('id', 'projects-container');
+  heading.textContent = 'Projects';
+  projectsHeader.appendChild(heading);
+  projectsContainer.append(projectsHeader);
+  storage.projects.forEach((project) => {
+    const projectMenuLink = menuLink(
+      project.title,
+      storage.projects.filter((proj) => proj.id === project.id).length,
+      randomFillPicker()
+    );
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = CloseIconRed;
+    deleteIcon.classList.add('project-delete-icon');
+    projectMenuLink.prepend(deleteIcon);
+    projectsContainer.appendChild(projectMenuLink);
+  });
+
+  function randomFillPicker() {
+    const randomIndex = Math.floor(Math.random() * fillIcons.length);
+    return fillIcons[randomIndex];
+  }
+
+  nav.appendChild(projectsContainer);
 
   return nav;
 };
